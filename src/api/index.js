ad80e5aba5ead37946173c710b558b9c89f6a04e
dlim <dlim87@gmail.com>
2018-11-21 14:04:45 -0800

    deploy5

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,14 @@
 // the address of our rails backend, saved as a constant value, because we never want to accidently change it
-const BASE = 'http://localhost:3000'
+const BASE = 'https://calm-cliffs-63936.herokuapp.com'
 
 let getDucks = function() {
     // the function name getCats is intended to remind you of the restful rails route --> GET '/cats'.
     return fetch(BASE + '/ducks') // this would be equivalent to going to localhost:3000/cats in your browser. Do that - - what do you see?
+        console.log(BASE);
         .then((resp) => {
             // resp will be whatever you saw on the page localhost:3000/cats, it is the result of our fetch call
             let json = resp.json() // we want to make sure what we have is just the json part of the response
+            console.log(json);
             return json
         })
 }
