4bdffb49603637d8043292b46081cdacf4df457d
dlim <dlim87@gmail.com>
2018-11-21 14:38:27 -0800

    deploy11

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,7 @@ const BASE = 'https://calm-cliffs-63936.herokuapp.com'
 let getDucks = function() {
   console.log(BASE);
     // the function name getCats is intended to remind you of the restful rails route --> GET '/cats'.
-    return fetch(BASE + '/ducks', {
-      method: 'GET',
-      headers: {
-        'Access-Control-Allow-Origin':'*'
-      },
-    }) // this would be equivalent to going to localhost:3000/cats in your browser. Do that - - what do you see?
+    return fetch(BASE + '/ducks') // this would be equivalent to going to localhost:3000/cats in your browser. Do that - - what do you see?
         .then((resp) => {
             // resp will be whatever you saw on the page localhost:3000/cats, it is the result of our fetch call
             let json = resp.json() // we want to make sure what we have is just the json part of the response
